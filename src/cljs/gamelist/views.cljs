daf6d1161c382b6753c3154192c9d9a8f2e2b1c8
Martin Collberg <martin.collberg@combination.se>
2018-07-29 23:25:34 +0200

    Divify

--- src/cljs/gamelist/views.cljs
+++ src/cljs/gamelist/views.cljs
@@ -5,20 +5,24 @@
    [gamelist.subs :as subs]
    ))
 
+
+(defn div-game
+  [game]
+  [:div game])
+
+(defn list-items
+  [data]
+  (for [g data]
+    (div-game g)))
+
 (defn div-game-list
   [games]
-  [:p "test: "])
+  [:div
+   (list-items games)
+   ])
 
 (defn main-panel []
   (let [name (rf/subscribe [::subs/name])]
     [:div
      [:h1 "Game list: " @name]
-     (div-game-list games/game-list)
-     ]))
-
-(defn list-items
-  [data]
-  "Generates a sequence of :li elements. Adding value of each item as metadata
-  (required for re-frame)"
-  (for [i data]
-    ^{:key i} [:li i]))
+     (div-game-list games/game-list)]))
