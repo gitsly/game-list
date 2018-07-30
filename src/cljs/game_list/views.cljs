34d7ff1ebb03bfa0487142fa9e46cf1b19727fb4
Martin Collberg <martin.collberg@combination.se>
2018-07-30 15:43:12 +0200

    Add changes from old-master manually

--- src/cljs/game_list/views.cljs
+++ src/cljs/game_list/views.cljs
@@ -1,15 +1,32 @@
 (ns game-list.views
   (:require
-   [re-frame.core :as re-frame]
+   [re-frame.core :as rf]
+   [game-list.games :as games]
    [game-list.subs :as subs]
    ))
 
-(defn apan
+;; '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+(defn rand-color
   []
-  2)
+  nil)
+
+(defn div-game
+  [game]
+  ^{:key game} [:div game])
+
+(defn list-items
+  [data]
+  (for [g data]
+    (div-game g)))
+
+(defn div-game-list
+  [games]
+  [:div
+   (list-items games)
+   ])
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
+  (let [name (rf/subscribe [::subs/name])]
     [:div
-     [:h1 "Hello from " @name]
-     ]))
+     [:h1 "Game list: " @name]
+     (div-game-list games/game-list)]))
