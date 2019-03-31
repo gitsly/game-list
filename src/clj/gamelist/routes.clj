efad8bbd1c02fc80951663476c6de4bd57a45b09
Martin Collberg <martin.collberg@gmail.se>
2019-03-31 13:16:46 +0200

    trying out wrap-reload, doesn't seem to work, but definition of welcome can be injected with cider runtime!

--- src/clj/gamelist/routes.clj
+++ src/clj/gamelist/routes.clj
@@ -2,16 +2,17 @@
   (:require [clojure.java.io :as io]
             [compojure.core :refer [ANY GET PUT POST DELETE routes]]
             [compojure.route :refer [resources]]
-            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.reload :refer [wrap-reload]] ;; https://practicalli.github.io/clojure-webapps/middleware-in-ring/wrap-reload.html
             [ring.util.response :refer [response]]))
 
-(System/getProperty "user.dir")
-(slurp "test.txt")
 
 ;; This will work as reload after modifying routes (server-side)
 (do
   (user/stop)
   (user/go))
+
+(defn welcome [a]
+  (str "Who" a))
 
 (defn home-routes [endpoint]
   (routes
@@ -29,8 +30,12 @@
    ;; the current working directory seems to be the root of the clojure project
    ;; (where project.clj lies)
    (GET "/slurp" _
-        (str (slurp "test.txt")))
+        (let [file "test.txt"
+              currpath (System/getProperty "user.dir")]
+          (str currpath ": " (slurp file))))
    
+   (GET "/wrap" _
+        (wrap-reload #'welcome))
    
    (GET "/test/:var1" [var1]
         (str "<html> <body> <p>var1: " var1 "</p> </body> </html>"))
