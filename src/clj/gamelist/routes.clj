4e116624d10e977c0830c710721946a3739f3866
Martin Collberg <martin.collberg@gmail.se>
2019-03-31 11:36:26 +0200

    working parameter passing

--- src/clj/gamelist/routes.clj
+++ src/clj/gamelist/routes.clj
@@ -21,7 +21,10 @@
             (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
    
    (GET "/test" x
-        (str "<html> <body> <p>Isolation rattle snarp</p> </body> </html>"))
+        (str "<html> <body> <p>Isolation rattle snake</p> </body> </html>"))
+   
+   (GET "/test/:var1" [var1]
+        (str "<html> <body> <p>var1: " var1 "</p> </body> </html>"))
    
    (resources "/")))
 
