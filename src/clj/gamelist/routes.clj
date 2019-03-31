235bd3c90957199ac5881d975117d9a1ca7f26a1
Martin Collberg <martin.collberg@gmail.se>
2019-03-31 12:59:21 +0200

    Working reading from server's root folder and put on page

--- src/clj/gamelist/routes.clj
+++ src/clj/gamelist/routes.clj
@@ -5,6 +5,8 @@
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.util.response :refer [response]]))
 
+(System/getProperty "user.dir")
+(slurp "test.txt")
 
 ;; This will work as reload after modifying routes (server-side)
 (do
@@ -22,6 +24,13 @@
    
    (GET "/test" x
         (str "<html> <body> <p>Isolation rattle snake</p> </body> </html>"))
+
+   ;; Read from file and put on page:
+   ;; the current working directory seems to be the root of the clojure project
+   ;; (where project.clj lies)
+   (GET "/slurp" _
+        (str (slurp "test.txt")))
+   
    
    (GET "/test/:var1" [var1]
         (str "<html> <body> <p>var1: " var1 "</p> </body> </html>"))
