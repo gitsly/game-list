ef4cae0d29518c9236d7b2011ebe9640eb699669
Martin Collberg <martin.collberg@gmail.se>
2019-04-04 20:05:41 +0200

    Closing in on the byte input stream

--- src/clj/gamelist/routes.clj
+++ src/clj/gamelist/routes.clj
@@ -13,52 +13,33 @@
   (user/stop)
   (user/go))
 
-(defn welcome [a]
-  (str "whohaa: " a))
+;; (spit "out.txt" (slurp body))
 
 (defn add-game
   [request]
-  (let [body (:body request)
-        content (.bytes (body))]
+  (let [body (:body request)]
     body))
+
 
 ;; org.httpkit.BytesInputStream 
 
-
 ;; https://stackoverflow.com/questions/3488353/whats-the-big-idea-behind-compojure-routes
 (defn home-routes [endpoint]
-  (routes
+(routes
+ 
+ (PUT "/addgame" request
+   (str "<html> <body> <p>" (add-game request) "</p> </body> </html>"))
 
-   (PUT "/addgame" request
-     (-> (add-game request)
-         (assoc :headers {"Content-Type" "application/json"})
-         (assoc :headers {"Accept" "application/json"})))))
+ (GET "/" _
+   (-> "public/index.html"
+       io/resource
+       io/input-stream
+       response
+       (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
 
-(GET "/" _
-  (-> "public/index.html"
-      io/resource
-      io/input-stream
-      response
-      (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
-  
-  (GET "/test" x
-    (str "<html> <body> <p>Isolation rattle snake</p> </body> </html>"))
+ (GET "/test/:var1" [var1]
+   (str "<html> <body> <p>var1: " var1 "</p> </body> </html>"))
 
-  ;; Read from file and put on page:
-  ;; the current working directory seems to be the root of the clojure project
-  ;; (where project.clj lies)
-  (GET "/slurp" _
-    (let [file "test.txt"
-          currpath (System/getProperty "user.dir")]
-      (str currpath ": " (slurp file))))
-  
-  
-  (GET "/wrap" r
-    (welcome r))
-  
-  (GET "/test/:var1" [var1]
-    (str "<html> <body> <p>var1: " var1 "</p> </body> </html>"))
-  
-  (resources "/")))
+ (resources "/")))
 
 
