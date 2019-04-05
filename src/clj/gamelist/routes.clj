cccfee381f5e928aa7f4b1f21583b90fd0b8fb67
Martin Collberg <martin.collberg@gmail.se>
2019-04-05 17:28:49 +0200

    Update to disable temporary start/stop

--- src/clj/gamelist/routes.clj
+++ src/clj/gamelist/routes.clj
@@ -1,20 +1,21 @@
 (ns gamelist.routes
   (:require [clojure.java.io :as io]
             [clojure.data.json :as json]
-            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.json :refer :all]
             [ring.util.response :refer [response]]
+            [compojure.handler :as handler]
             [compojure.core :refer [ANY GET PUT POST DELETE routes]]
             [compojure.route :refer [resources]]
             [ring.util.response :refer [response]]))
 
 ;; This will work as reload after modifying routes (server-side)
-(do
-  (user/stop)
-  (user/go))
+;; (do
+;;   (user/stop)
+;;   (user/go))
 
 (defn add-game-handler
   [request]
-  "hep")
+  (str "Hep: " (:body request)))
 
 (defn home-routes [endpoint]
   (routes
@@ -33,3 +34,10 @@
      (str "<html> <body> <p>var1: " var1 "</p> </body> </html>"))
 
    (resources "/")))
+
+;; is compojure.handler deprecated?
+(def app
+  (-> (handler/api home-routes)
+      (wrap-json-body)
+      (wrap-json-params)
+      (wrap-json-response)))
