01e403f88c23c5342a9172a29f282aaf3a296a34
Jonas Collberg <jco@jco-arch.mydomain>
2019-04-05 20:18:12 +0200

    Add JSON middleware in the correct place
    Note that `wrap-json-params` is an alternative to `wrap-json-body`, and
    there is no need to use both.

--- src/clj/gamelist/config.clj
+++ src/clj/gamelist/config.clj
@@ -2,10 +2,13 @@
   (:require [environ.core :refer [env]]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [ring.middleware.gzip :refer [wrap-gzip]]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
             [ring.middleware.logger :refer [wrap-with-logger]]))
 
 (defn config []
   {:http-port  (Integer. (or (env :port) 10555))
    :middleware [[wrap-defaults api-defaults]
+                wrap-json-body
+                wrap-json-response
                 wrap-with-logger
                 wrap-gzip]})
