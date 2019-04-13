cbc65998704ff73ac84aefb7dfbf10638819c942
Martin Collberg <martin.collberg@gmail.se>
2019-04-13 18:33:17 +0200

    test add with non-guid id, seems to skip id generation in mongodb

--- src/clj/gamelist/routes.clj
+++ src/clj/gamelist/routes.clj
@@ -9,6 +9,9 @@
             [monger.collection :as mc])
   (:import [com.mongodb MongoOptions ServerAddress]))
 
+
+
+
 (defn test-connect []
   (println "Testing connect to mongo db")
   (let [^MongoOptions options (mg/mongo-options {:threads-allowed-to-block-for-connection-multiplier 300})
@@ -17,9 +20,16 @@
         db                    (mg/get-db conn "test")]
     db))
 
-;; (mc/insert-and-return (test-connect) "documents" {:name "John" :age 30})
-;; (test-connect)
+(mc/insert-and-return (test-connect) "documents" {:name "Kikana" :age 32})
+(test-connect)
 
+(dissoc {:id 0, :name "Kepler"} :id)
+
+(dissoc {"id" 0, "name" "Kepler"} "id")
+
+(json/read-str "{\"a\":1,\"b\":2}" :key-fn keyword)
+
+(json/read-str (str {"id" 16, "name" "Abatorrr21"}) :key-fn keyword)
 
 ;; This will work as reload after modifying routes (server-side)
 ;; (do
@@ -31,27 +41,34 @@
   (if-let [type (get-in request [:headers "content-type"])]
     (not (empty? (re-find #"^application/(.+\+)?json" type)))))
 
+
 (defn add-game-handler
   [request]
   (let [db (test-connect)
-        game (:body request)]
-    (mc/insert (test-connect) "games" game)
-    (str "JSON request: " game)))
+        game (json/read-str (str (:body request)) :key-fn keyword)
+        game (dissoc game :id)
+        game (assoc game :id)
+        db-result (mc/insert-and-return (test-connect) "games" game)
+        obj-id (:_id db-result)]
+    (str "obj-id: " obj-id)))
+
+;; (str "Insert: " ", ID: " obj-id)
+
 
 (defn home-routes [endpoint]
-  (routes
+(routes
 
-   (POST "/addgame" request
-     (add-game-handler request))
+ (POST "/addgame" request
+   (add-game-handler request))
 
-   (GET "/" _
-     (-> "public/index.html"
-         io/resource
-         io/input-stream
-         response
-         (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
+ (GET "/" _
+   (-> "public/index.html"
+       io/resource
+       io/input-stream
+       response
+       (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
 
-   (GET "/test/:var1" [var1]
-     (str "<html> <body> <p>var1: " var1 "</p> </body> </html>"))
+ (GET "/test/:var1" [var1]
+   (str "<html> <body> <p>var1: " var1 "</p> </body> </html>"))
 
-   (resources "/")))
+ (resources "/")))
