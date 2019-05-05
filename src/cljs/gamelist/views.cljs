fa9717d16d6ec1946fcfa55c1cd0b4d5dcc43dd4
Martin Collberg <martin.collberg@gmail.se>
2019-05-05 14:39:58 +0200

    Add game refactored to flexbox

--- src/cljs/gamelist/views.cljs
+++ src/cljs/gamelist/views.cljs
@@ -5,7 +5,7 @@
    [gamelist.events :as events]
    [goog.object :as gobject]
    [clojure.string :as string]
-   [re-com.core   :refer [button h-box v-box box gap line label title slider checkbox input-text horizontal-bar-tabs vertical-bar-tabs p]]
+   [re-com.core   :refer [input-text button h-box v-box box gap line label title slider checkbox input-text horizontal-bar-tabs vertical-bar-tabs p]]
    [re-com.misc   :refer [slider-args-desc]]
    [reagent.core :as reagent]))
 
@@ -64,22 +64,6 @@
      :children [(for [game @games]
                   ^{:key (:_id game)} [:div (div-game-common game)])]]))
 
-(defn div-add-game
-  []
-  [:div {:id "the-div" :style {:background-color "#f3e0bb"}}
-
-   [:input {:id "the-input" :class "text"}]
-   [:button
-    {:on-click
-     ;; Note, the .-target property refers to the event (javascript)
-     #(rf/dispatch [::events/add-game
-                    (-> %
-                        .-target
-                        .-parentNode
-                        (.querySelector "#the-input")
-                        .-value)])}
-    add-game-text]
-   ])
 
 (defn div-loading
   []
@@ -87,39 +71,41 @@
         games (rf/subscribe [::subs/games])]
     [:div "Loading: " (str @loading)]))
 
-
-;; (defn main-panel []
-;;   (let [name (rf/subscribe [::subs/name])]
-;;     [v-box
-;;      :children [[:h1 "Game list: " @name]
-;;                 (div-game-list)
-;;                 (div-add-game)
-;;                 (div-loading)
-;;                 [v-box :children ["apan " "bepan " "cepan "]]]]))
+(defn add-game-box
+  []
+  (let [text-val (reagent/atom "")]
+    [input-text
+     :model            text-val
+     :width            "300px"
+     :placeholder      "Game name"
+     :change-on-blur?  true
+     :on-change        #((rf/dispatch [::events/add-game %])
+                         (reset! text-val %))]))
 
 (defn main-panel
-  []
-  (let [name (rf/subscribe [::subs/name])
-        games (rf/subscribe [::subs/games])
-        game (first @games)]
-    [v-box
-     :style {:background-color "#AAAAAA" }
-     :gap "2px"
-     :children [[box :child (str "Test:" @name) ]
-                [h-box
-                 :style {:background-color "#AAAAFF" }
-                 :height "100px"
-
-                 :children [[box :size "70px" :child "Nav"]
-                            [button
-                             :label "Test"
-                             :on-click #(rf/dispatch [::events/test "Bullen"])]
-                            [box
-                             :style {:background-color "#AAFFAA" }
-                             :size "1" :child "Content"]]]
-                [box
-                 :style {:background-color "#FFAAAA" }
-                 :child "Footer"]]]))
+[]
+(let [name (rf/subscribe [::subs/name])
+      games (rf/subscribe [::subs/games])
+      game (first @games)]
+  [v-box
+   :style {:background-color "#AAAAAA" }
+   :gap "2px"
+   :children [[box :child (str "Haha:" @name) ]
+              [h-box
+               :style {:background-color "#AAAAFF" }
+               :height "100px"
+               :children [[box :size "70px" :child "Nav"]
+                          [label :label "[input-textarea ... ]"]
+                          (add-game-box)
+                          [button
+                           :label "Test"
+                           :on-click #(rf/dispatch [::events/test "Bullen"])]
+                          [box
+                           :style {:background-color "#AAFFAA" }
+                           :size "1" :child "Content2"]]]
+              [box
+               :style {:background-color "#FFAAAA" }
+               :child "Footer"]]]))
 
 
 ;; Compiling build :app to "dev-target/public/js/compiled/gamelist.js" from
@@ -129,18 +115,18 @@
 
 (let [a [ 1  2  3]
       b ["a" "b" "c"]]
-  (map #(zipmap [:digit :letter] [% %2]) a b))
+(map #(zipmap [:digit :letter] [% %2]) a b))
 
 (let [a {:name "ninja" :stamina 18 }
-b {:name "ninja" :stamina 15 }
-[only-a only-b both] (clojure.data/diff a b)]
+      b {:name "ninja" :stamina 15 }
+      [only-a only-b both] (clojure.data/diff a b)]
 both)
 
 
 (let [id 1
-data [{:id 0 :name "bength"}
-      {:id 1 :name "alice"}
-      {:id 2 :name "lisa"}]
-modded (remove #(= id (:id %)) data)]
+      data [{:id 0 :name "bength"}
+            {:id 1 :name "alice"}
+            {:id 2 :name "lisa"}]
+      modded (remove #(= id (:id %)) data)]
 modded
 )
