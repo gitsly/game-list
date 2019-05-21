156e4cdd0087adeb38ded5cb2179ae8ed05044a7
Martin Collberg <martin.collberg@gmail.se>
2019-05-21 19:49:50 +0200

    Start visualizing data

--- src/cljs/gamelist/views/chat.cljs
+++ src/cljs/gamelist/views/chat.cljs
@@ -13,7 +13,22 @@
             [re-com.misc   :refer [slider-args-desc]]
             [reagent.core :as reagent]))
 
+;; content
+;; user
+;; added
+
+(defn chat-panel-children
+  [entries]
+  (for [i (range 3)]
+    ^{:key i} [h-box :children [(str "test" i)]]))
+
+
 (defn chat-panel
   []
-  (let [chat-info (rf/subscribe [::subs/chat])]
-    [:p "TODO: views.chatish2"]))
+  (let [chat-info (rf/subscribe [::subs/chat])
+        entries (:entries chat-info)]
+    [v-box :children [(chat-panel-children entries)]]))
+
+;; (for [entry entries]
+;;   [:p "TODO: views.chatish2"]
+;;   )
