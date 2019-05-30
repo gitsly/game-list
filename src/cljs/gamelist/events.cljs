5ac3055ca3c65b4c55a5977e9323b6c778f6bc84
Martin Collberg <martin.collberg@gmail.se>
2019-05-30 13:43:33 +0200

    Separate chat event functionality into chat.cljs

--- src/cljs/gamelist/events.cljs
+++ src/cljs/gamelist/events.cljs
@@ -2,6 +2,7 @@
   (:require
    [re-frame.core :as rf]
    [gamelist.db :as db]
+   [gamelist.events.chat :as chat]
    [gamelist.utils :as utils]
    [cljs-http.client :as http]
    [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]
@@ -26,29 +27,12 @@
   (go (let [response (<! (http/get "list/games"))]
         (rf/dispatch [::get-all-games-response response]))))
 
-;; TODO: move to events/chat.cljs
-(defn get-chat
-  "takes id of the chat to fetch single string
-  stores result in 'chat' subscription of app-db"
-  [session]
-  (let [payload (utils/json-request {:session session})
-        url (str "list/chat/" session)]
-    ;; (println "GET-CHAT:" url)
-    (go (let [response (<! (http/get url))]
-          (rf/dispatch [::get-chat-response response])))))
-
-;; Get this to work...
-(rf/reg-event-db
-  ::get-chat
-  (fn [_ [event-name session]]
-    (println "Getting chat content: " session)
-    (get-chat session)))
 
 (rf/reg-event-db
   ::initialize-db
   (fn [_ _]
     (get-all-games)
-    (get-chat 'test)
+    (chat/get-chat 'main)
     db/default-db))
 
 (rf/reg-event-db ;; register-handler has been renamed to reg-event-db
@@ -62,16 +46,6 @@
       (-> db
           (assoc :user user)
           (assoc :games games)))))
-
-(rf/reg-event-db
-  ::get-chat-response
-  (fn
-    [db [_ response]]
-    (let [body (-> response :body)
-          chat body]
-      (println "client: get-chat-response: " body)
-      (-> db
-          (assoc :chat chat)))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -185,34 +159,6 @@
       (-> db
           (assoc :loading? false)
           (assoc :games (conj games game))))))
-
-;; Chat impl: TODO: move to events/chat.cljs --------------------
-
-(rf/reg-event-db
-  ::add-chat
-  (fn [db
-       [event-name
-        session
-        content]]
-    (println "add-chat-event: " session ", content: " content)
-    (let [msg {:entry {:content content :user (:user db) }
-               :session session}
-          url "list/addchat"
-          payload (utils/json-request msg)]
-      (go (let [response (<! (http/put url payload))]
-            (rf/dispatch [::add-chat-response response])))
-      (-> db
-          (assoc :loading? true)))))
-
-(rf/reg-event-db
-  ::add-chat-response
-  (fn
-    [db [_ response]]
-    (let [chat (:body response)]
-      ;; (println "client: add-chat-response: " chat)
-      (-> db
-          (assoc :loading? false)
-          (assoc :chat chat)))))
 
 ;;--------------------
 
---
+++ src/cljs/gamelist/events/chat.cljs
@@ -0,0 +1,64 @@
+(ns gamelist.events.chat
+  (:require
+   [re-frame.core :as rf]
+   [gamelist.db :as db]
+   [gamelist.utils :as utils]
+   [cljs-http.client :as http]
+   [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]
+   [cljs.core.async :as async :refer [<! >!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+;; TODO: move to events/chat.cljs
+(defn get-chat
+  "takes id of the chat to fetch single string
+  stores result in 'chat' subscription of app-db"
+  [session]
+  (let [payload (utils/json-request {:session session})
+        url (str "list/chat/" session)]
+    ;; (println "GET-CHAT:" url)
+    (go (let [response (<! (http/get url))]
+          (rf/dispatch [::get-chat-response response])))))
+
+;; Get this to work...
+(rf/reg-event-db
+  ::get-chat
+  (fn [_ [event-name session]]
+    (println "Getting chat content: " session)
+    (get-chat session)))
+
+(rf/reg-event-db
+  ::get-chat-response
+  (fn
+    [db [_ response]]
+    (let [body (-> response :body)
+          chat body]
+      (println "client: get-chat-response: " body)
+      (-> db
+          (assoc :chat chat)))))
+
+
+(rf/reg-event-db
+  ::add-chat
+  (fn [db
+       [event-name
+        session
+        content]]
+    (println "add-chat-event: " session ", content: " content)
+    (let [msg {:entry {:content content :user (:user db) }
+               :session session}
+          url "list/addchat"
+          payload (utils/json-request msg)]
+      (go (let [response (<! (http/put url payload))]
+            (rf/dispatch [::add-chat-response response])))
+      (-> db
+          (assoc :loading? true)))))
+
+(rf/reg-event-db
+  ::add-chat-response
+  (fn
+    [db [_ response]]
+    (let [chat (:body response)]
+      ;; (println "client: add-chat-response: " chat)
+      (-> db
+          (assoc :loading? false)
+          (assoc :chat chat)))))
